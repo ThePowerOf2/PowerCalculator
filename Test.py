f27fb8f36b52c51b6f116b28486fd2a1a1dee53d
ThePowerOf2 <coreyr147@outlook.ie>
2017-01-27 16:30:16 +0000

    Test File to Run the PowerCalculator.

---
+++ Test.py
@@ -0,0 +1,24 @@
+# Importing the Calculator.
+import PowerCalculator
+
+# Declaring variables.
+Number = 0
+Power = 0
+
+# Asking the user to input a number.
+print "Please enter the Number you wish to use: ",
+while not Number: # If they enter anything other than an integer it will return 'Invalid Number'.
+    try:
+        Number = int(raw_input())
+    except ValueError:
+        print 'Invalid Number'
+print "Please enter the Power you wish to use: ",
+while not Power: # If they enter anything other than an integer it will return 'Invalid Number'.
+    try:
+        Power = int(raw_input())
+    except ValueError:
+        print 'Invalid Number'
+
+# Printing out the result to the user.
+print "Result = ",
+PowerCalculator.Power(Number,Power)
